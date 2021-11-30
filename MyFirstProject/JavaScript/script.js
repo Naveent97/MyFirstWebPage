cb93f083555eca04e89e1737011c7b870153cc96
Naveent97 <tnaveen1997@gmail.com>
2021-11-30 20:59:22 +0530

    Experimental updation

--- MyFirstProject/JavaScript/script.js
+++ MyFirstProject/JavaScript/script.js
@@ -7,12 +7,12 @@ function getValueFromTextArea()
 {
   var val = document.getElementById("dairyArea").value;
   var tab = document.getElementById("mainTable");
-
+  var today = new Date();
   var nodeTr = document.createElement('tr');
- var newLabel = document.createElement("label");
- newLabel.setAttribute("for","newData");
- newLabel.innerHTML = val;
- nodeTr.appendChild(newLabel);
- tab.insertBefore(newLabel,tab.childNodes[0]);
+  var newLabel = document.createElement("label");
+  newLabel.setAttribute("for", "newData");
+  newLabel.innerHTML = val;
+  nodeTr.appendChild(newLabel);
+  tab.insertBefore(newLabel, tab.childNodes[1]);
 
 }
