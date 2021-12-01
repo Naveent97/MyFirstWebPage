a05bd68abad33cf8227345d5c8bbe1c4b7d81062
Naveent97 <tnaveen1997@gmail.com>
2021-12-01 20:43:37 +0530

    Javascript Update

--- MyFirstProject/JavaScript/script.js
+++ MyFirstProject/JavaScript/script.js
@@ -6,13 +6,15 @@ function clickClear(){
 function getValueFromTextArea()
 {
   var val = document.getElementById("dairyArea").value;
-  var tab = document.getElementById("mainTable");
   var today = new Date();
+
+  let tr1 = document.getElementById('firstRow');
+  var tab = document.getElementById("mainTable");
   var nodeTr = document.createElement('tr');
   var newLabel = document.createElement("label");
   newLabel.setAttribute("for", "newData");
   newLabel.innerHTML = val;
   nodeTr.appendChild(newLabel);
-  tab.insertBefore(newLabel, tab.childNodes[1]);
+  tab.insertBefore(newLabel, tr1);
 
 }
